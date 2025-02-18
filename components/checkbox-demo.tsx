7fbb9b4547c327ad49cf9c74b0128e67c8a051f1
triginarsa <adi@cakeplabs.com>
2025-02-18 15:25:10 +0800

    chores: add demo on input, add checkbox variant

--- components/checkbox-demo.tsx
+++ components/checkbox-demo.tsx
@@ -5,9 +5,15 @@ import { Label } from "./ui/label";
 
 export function CheckboxDemo() {
   return (
-    <div className="flex items-center space-x-2">
-      <Checkbox id="terms" />
-      <Label htmlFor="terms">I agree to the terms and conditions</Label>
-    </div>
+    <>
+      <div className="flex items-center space-x-2">
+        <Checkbox id="terms" />
+        <Label htmlFor="terms">I agree to the terms and conditions</Label>
+      </div>
+      <div className="flex items-center space-x-2">
+        <Checkbox id="newsletter" variant="secondary" />
+        <Label htmlFor="newsletter">Subscribe to newsletter</Label>
+      </div>
+    </>
   );
 }
--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -2,25 +2,44 @@
 
 import * as React from "react";
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
 
+const checkboxVariants = cva(
+  "peer h-3 w-3 shrink-0 border focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default:
+          "border-border-light rounded-xs data-[state=checked]:bg-primary data-[state=checked]:text-foreground data-[state=checked]:border-foreground",
+        secondary:
+          "border-t-[#292c21] border-l-[#292c21] border-r-[#8c9284] bg-muted border-b-[#8c9284] data-[state=checked]:text-accent-foreground",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+interface CheckboxProps
+  extends React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>,
+    VariantProps<typeof checkboxVariants> {}
+
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
->(({ className, ...props }, ref) => (
+  CheckboxProps
+>(({ className, variant, ...props }, ref) => (
   <CheckboxPrimitive.Root
     ref={ref}
-    className={cn(
-      "h-4 w-4 peer shrink-0 rounded-xs border border-border-light focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-foreground data-[state=checked]:border-foreground",
-      className
-    )}
+    className={cn(checkboxVariants({ variant, className }))}
     {...props}
   >
     <CheckboxPrimitive.Indicator
       className={cn("flex items-center justify-center text-current")}
     >
-      <Check className="h-4 w-4" />
+      <Check className="h-3 w-3" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ));
@@ -36,7 +55,7 @@ const Check: React.FC<CheckProps> = ({ className, ...props }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 10 10"
-    className={cn("h-4 w-4 fill-current", className)}
+    className={cn("h-3 w-3 fill-current", className)}
     {...props}
   >
     <path d="M2 6h1v1H2m1-2h1v1H3M2 5h1v1H2m0-2h1v1H2m1 1h1v1H3m0 0h1v1H3m1-2h1v1H4m0 0h1v1H4m0 0h1v1H4m1-2h1v1H5m0-2h1v1H5m0-2h1v1H5m1-2h1v1H6m0 0h1v1H6m0 0h1v1H6m1-2h1v1H7m0-2h1v1H7m0-2h1v1H7m1 0h1v1H8m0-2h1v1H8m0-2h1v1H8"></path>
